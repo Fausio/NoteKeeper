c978a30191119cfaa43daf695d3e94f061253d26
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-09-01 14:52:13 +0200

    PLDataStore interface

---
+++ NoteKeeper/NoteKeeper/Services/PLDataStore.cs
@@ -0,0 +1,16 @@
+﻿using NoteKeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteKeeper.Services
+{
+    public interface PLDataStore
+    {
+         Task<String> AddNoteAsync(Note model);
+         Task<Boolean> UpdateNoteAsync(Note model);
+         Task<IList<Note>> GetNotesAsync();
+         Task<List<string>> GetCourseAsync();
+    }
+}
